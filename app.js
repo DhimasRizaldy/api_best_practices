bd24cf3a3615a8c50d4920db224c9dfc2dde661e
DhimasRizaldy <dtekchannel02@gmail.com>
2023-10-04 17:50:25 +0700

    dimas: upload project api crud v1

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { PORT } = process.env;
+
+const v1Router = require('./routes/v1');
+
+app.use(express.json());
+app.use('/api/v1', v1Router);
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}/api/v1/`));
